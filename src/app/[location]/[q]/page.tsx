e662f6ac10613ebeb14e639adf3530ef78a80ba6
guglastican <guglastican@users.noreply.github.com>
2025-02-23 20:27:22 +0100

    Update page.tsx

--- src/app/[location]/[q]/page.tsx
+++ src/app/[location]/[q]/page.tsx
@@ -1,6 +1,6 @@
-import Header from "@/components/Header";
-import RestaurantItem from "@/components/RestaurantItem";
-import { getAllTags, locations, searchRestaurants } from "@/data/restaurants";
+import { Header } from '@/components/Header';
+import { HotelItem } from '@/components/HotelItem';
+import { getAllTags, locations, searchRestaurants } from "@/data/hotels";
 import { Metadata } from "next";
 import { cache } from "react";
 
