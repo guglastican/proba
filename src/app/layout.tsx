8e766f8a43afbb1f281fd4a493318c8d65762588
guglastican <guglastican@users.noreply.github.com>
2025-02-24 15:56:07 +0100

    GTM Tag Layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist } from "next/font/google";
 import "./globals.css";
+import { GoogleTagManager } from "@next/third-parties/google";
+import Script from "next/script";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -23,3 +25,15 @@ export default function RootLayout({
     </html>
   );
 }
+//Google Manager Tag
+{process.env.NODE_ENV === "production" && (
+  <>
+    <GoogleTagManager gtmId="GTM-KQ7B5BL5" />
+    <Script
+      async
+      src={`https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-YOUR-ID`}
+      crossOrigin="anonymous"
+      strategy="afterInteractive"
+    />
+  </>
+)}
