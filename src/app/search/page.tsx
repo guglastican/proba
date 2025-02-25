910a31f2c8c9d5c158ca73e4f39ca5bb1d480416
guglastican <guglastican@users.noreply.github.com>
2025-02-25 15:14:43 +0100

    update title

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -37,11 +37,11 @@ async function Results({ q, location }: ResultsProps) {
 
   return (
     <main className="container mx-auto space-y-8 px-4 py-8">
-      <p className="text-center font-semibold bg-blue-500 text-white p-2 rounded">
+      <p className="text-center font-semibold">
         Showing {results.length} results for {`"${q}"`} near {location}
       </p>
       <h1 className="text-center text-3xl font-bold mt-4 mb-6">
-        {q.charAt(0).toUpperCase() + q.slice(1)} Hotels in {location}
+        {q.charAt(0).toUpperCase() + q.slice(1)} in {location}
       </h1>
       <div className="grid grid-cols-1 gap-6 md:grid-cols-2 lg:grid-cols-3">
         {results.map((hotel) => (
