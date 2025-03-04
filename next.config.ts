cb6d299107066b2482d8eb940d2de42d9112a22e
guglastican <guglastican@users.noreply.github.com>
2025-03-04 20:43:47 +0100

    update

--- next.config.ts
+++ next.config.ts
@@ -11,6 +11,14 @@ const nextConfig: NextConfig = {
         protocol: "https",
         hostname: "images.trvl-media.com",
       },
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+      },
+      {
+        protocol: "https",
+        hostname: "developers.google.com",
+      },
     ],
   },
 };
