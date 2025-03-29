30e5b75537235962357339aa368d1aed27f97538
guglastican <guglastican@users.noreply.github.com>
2025-03-29 13:10:18 +0100

    update

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,6 @@
 import type { Metadata } from "next";
 import { Geist } from "next/font/google";
 import "./globals.css";
-import { GoogleTagManager } from "@next/third-parties/google";
 import { sanitizeUrl } from "@/lib/utils";
 import Footer from "@/components/Footer";
 
