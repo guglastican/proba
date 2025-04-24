08142b16fcd26841c8d1addbd5912e3e6394bdc0
guglastican <guglastican@users.noreply.github.com>
2025-04-24 11:56:10 +0200

    update

--- next.config.ts
+++ next.config.ts
@@ -15,10 +15,9 @@ const nextConfig = {
   async redirects() {
     return [
       {
-        source: 'https://romantic-vacations-destinations.com/new-york/',
-        destination: 'https://www.romantic-vacations-destinations.com/New%20York%2C%20NY/hot%20tub', 
-        permanent: true, // 301 redirect (permanent)
-        // use false for 307 (temporary) redirects
+        source: '/new-york',
+        destination: '/New%20York%2C%20NY/hot%20tub', 
+        permanent: true,
       },
       {
         // Using path matching parameters
