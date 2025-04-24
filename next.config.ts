20e61bf9c845619b2c368b3ad0e1fa044321d149
guglastican <guglastican@users.noreply.github.com>
2025-04-24 12:56:47 +0200

    update

--- next.config.ts
+++ next.config.ts
@@ -21,93 +21,10 @@ const nextConfig = {
       },
       {
         source: '/romantic-getaways-in-eureka-springs-arkansas',
-        destination: '',
+        destination: '/',
         permanent: true,
       },
-      {
-        // Source: https://romantic-vacations-destinations.com/hotels-with-jacuzzi-in-room-tucson/
-        source: '/hotels-with-jacuzzi-in-room-tucson/',
-        destination: '/', // Redirecting to homepage
-        permanent: true,
-      },
-      {
-        // Source: https://romantic-vacations-destinations.com/romantic-getaways-in-georgia-with-hot-tub/
-        source: '/romantic-getaways-in-georgia-with-hot-tub/',
-        destination: '/', // Redirecting to homepage
-        permanent: true,
-      },
-      {
-        // Source: https://romantic-vacations-destinations.com/romantic-getaways-ontario/
-        source: '/romantic-getaways-ontario/',
-        destination: '/', // Redirecting to homepage
-        permanent: true,
-      },
-      {
-        // Source: https://romantic-vacations-destinations.com/search/{search_term_string}/feed/rss2/
-        source: '/search/{search_term_string}/feed/rss2/',
-        destination: '/', // Redirecting to homepage
-        permanent: true,
-      },
-      {
-        // Source: https://romantic-vacations-destinations.com/blog/page/2/
-        source: '/blog/page/2/',
-        destination: '/', // Redirecting to homepage
-        permanent: true,
-      },
-      {
-        // Source: https://romantic-vacations-destinations.com/yankee-stadium
-        source: '/yankee-stadium',
-        destination: '/', // Redirecting to homepage
-        permanent: true,
-      },
-      {
-        // Source: https://romantic-vacations-destinations.com/tips-to-spend-your-leisure-in-summer/
-        source: '/tips-to-spend-your-leisure-in-summer/',
-        destination: '/', // Redirecting to homepage
-        permanent: true,
-      },
-      {
-        // Source: https://romantic-vacations-destinations.com/hotel-with-jacuzzi-in-room-tulsa/
-        source: '/hotel-with-jacuzzi-in-room-tulsa/',
-        destination: '/', // Redirecting to homepage
-        permanent: true,
-      },
-      {
-        // Source: https://romantic-vacations-destinations.com/traverse-city-hotels-with-jacuzzi-in-room/
-        source: '/traverse-city-hotels-with-jacuzzi-in-room/',
-        destination: '/', // Redirecting to homepage
-        permanent: true,
-      },
-      {
-        // Source: https://romantic-vacations-destinations.com/romantic-getaways-in-washington/
-        source: '/romantic-getaways-in-washington/',
-        destination: '/', // Redirecting to homepage
-        permanent: true,
-      },
-      {
-        // Source: https://romantic-vacations-destinations.com/romantic-picnic-ideas/
-        source: '/romantic-picnic-ideas/',
-        destination: '/', // Redirecting to homepage
-        permanent: true,
-      },
-      {
-        // Source: https://romantic-vacations-destinations.com/safety-tips-for-travellers/
-        source: '/safety-tips-for-travellers/',
-        destination: '/', // Redirecting to homepage
-        permanent: true,
-      },
-      {
-        // Source: https://romantic-vacations-destinations.com/oklahoma/
-        source: '/oklahoma/',
-        destination: '/', // Redirecting to homepage
-        permanent: true,
-      },
-      {
-        // Source: https://romantic-vacations-destinations.com/bronx-zoo
-        source: '/bronx-zoo',
-        destination: '/', // Redirecting to homepage
-        permanent: true,
-      },
+      
       {
         // Using path matching parameters
         source: '/blog/:slug',
